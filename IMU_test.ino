ca0c530d0cb8d2b5b403899566c879a8d68711b9
rbv188 <rbvgithub@gmail.com>
2019-04-23 08:17:33 -0400

    Update IMU_test.ino

--- IMU_test.ino
+++ IMU_test.ino
@@ -2,13 +2,15 @@
 #include <quaternion.h>
 #include <sensor_processing_lib.h>
 #include <vector_3d.h>
+
 const int MPU_addr=0x68;  // I2C address of the MPU-6050
-int16_t AcX=0,AcY=0,AcZ=1,Tmp,GyX=0,GyY=0,GyZ=0;
-unsigned long Start = 0;
-float delta,ax,ay,az,wx,wy,wz;
+int16_t AcX=0,AcY=0,AcZ=1,Tmp,GyX=0,GyY=0,GyZ=0,count=0;
+unsigned long Start = 0,loop_start,temp2;
+float delta,wx,wy,wz;
 euler_angles angles;
-Quaternion q, q_new, q_acc, q_gyro;
-vector_ijk virtual_gravity,sensor_gravity,temp;
+vector_ijk virtual_gravity,sensor_gravity,fused_vector;
+Quaternion q_acc;
+
 void setup(){
   Wire.begin();
   Wire.beginTransmission(MPU_addr);
@@ -21,35 +23,26 @@ void setup(){
   Wire.endTransmission(true);
   Wire.beginTransmission(MPU_addr);
   Wire.write(0x1C);  // ACCEL_CONFIG register
-  Wire.write(0x08);     // set to 0x10 (+- 4g)
+  Wire.write(0x08);     // set to 0x08 (+- 4g)
   Wire.endTransmission(true);
   Serial.begin(115200);
   Start = millis(); 
-  q_gyro = quaternion_initialize(1.0,0.0,0.0,0.0);
   virtual_gravity = vector_3d_initialize(0.0,0.0,-1.0);
   q_acc = quaternion_initialize(1.0,0.0,0.0,0.0);
+  loop_start = millis();
 }
 void loop(){
+  
   wx = 0.03*GyX;
   wy = 0.03*GyY;
   wz = 0.03*GyZ;
-  ax = float(AcX);
-  ay = float(AcY);
-  az = float(AcZ);
-
-  sensor_gravity.a = ax;
-  sensor_gravity.b = ay;
-  sensor_gravity.c = az;
-
-  sensor_gravity = vector_3d_normalize(sensor_gravity);
   
   delta = 0.001*(millis()-Start);
-  q_gyro = quaternion_from_gyro(wx,wy,wz,delta);
-  virtual_gravity = quaternion_rotate_vector(temp,q_gyro);
-  virtual_gravity = vector_3d_scale(virtual_gravity,15);
-  temp = vector_3d_sum(virtual_gravity,sensor_gravity);
-  temp = vector_3d_normalize(temp);
-  q_acc = quaternion_from_accelerometer(temp.a,temp.b,temp.c);
+  virtual_gravity = update_gravity_vector(fused_vector,wx,wy,wz,delta);
+  sensor_gravity = sensor_gravity_normalized(AcX,AcY,AcZ);
+  fused_vector = fuse_vector(virtual_gravity,sensor_gravity);
+  
+  q_acc = quaternion_from_accelerometer(fused_vector.a,fused_vector.b,fused_vector.c);
   angles = quaternion_to_euler_angles(q_acc);
 
   Start = millis();
@@ -70,10 +63,4 @@ void loop(){
   GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
   GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
   GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
-
-  /* Gyro bias
-  GyX -= 8;
-  GyY += 30;
-  GyZ -= 10;
-  */
 }
